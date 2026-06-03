dbbf7b28de4a0286e9cb8ae729cd87b27a3aec09
Hentsrakoto <rakotonavalonahents@gmail.com>
2026-06-03 16:11:51 +0300

    feat: Seo opt

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import PrerenderPlugin from 'vite-plugin-prerender' // ← pas de { }
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    PrerenderPlugin({
+      staticDir: fileURLToPath(new URL('./dist', import.meta.url)),
+      routes: ['/'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
